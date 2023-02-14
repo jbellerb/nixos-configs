334bbe2cc71d066cbe5b1800ec674bdfc8d82518
Jared Beller <jbellerb@vt.edu>
2023-02-13 19:33:33 -0500

    configure pounce-notify in pounce.networks.<name>

--- hosts/shanghai/modules/pounce.nix
+++ hosts/shanghai/modules/pounce.nix
@@ -1,6 +1,27 @@
 { config, pkgs, ... }:
 
-{
+let
+  away-msg = "Currently disconnected from bouncer. A notification has been sent.";
+
+  notify-script = network: ''
+    NETWORK="${network}"
+    WEBHOOK=$(cat "${config.sops.secrets.shanghai-pounce-webhook.path}")
+
+    if [ -z "$NOTIFY_CHANNEL" ]; then
+      TITLE="Private Message"
+      CONTEXT="$NOTIFY_NICK"
+    else
+      TITLE="Highlight"
+      CONTEXT="$NOTIFY_CHANNEL"
+    fi
+
+    ${pkgs.curl}/bin/curl \
+      -H "Content-Type: application/json" \
+      -d "{\"embeds\":[{\"title\":\"$TITLE | $NETWORK/$CONTEXT\",\"description\":\"$NOTIFY_TIME $NOTIFY_NICK: $NOTIFY_MESSAGE\"}]}" \
+      $WEBHOOK
+  '';
+
+in {
   sops.secrets.shanghai-pounce-certfp-libera = { owner = "pounce"; };
   sops.secrets.shanghai-pounce-certfp-oftc = { owner = "pounce"; };
   sops.secrets.shanghai-pounce-webhook = { owner = "pounce"; };
@@ -13,48 +34,29 @@
     user = "pounce";
     openFirewall = true;
 
-    networks.libera = {
-      host = "irc.libera.chat";
-      port = 6697;
-      nick = "waves";
-      client-cert = config.sops.secrets.shanghai-pounce-certfp-libera.path;
-      sasl-external = true;
-      join = "#openbsd,#nixos";
-      away = "Currently disconnected from bouncer. A notification has been sent.";
-    };
-
-    networks.oftc = {
-      host = "irc.oftc.net";
-      port = 6697;
-      nick = "waves";
-      client-cert = config.sops.secrets.shanghai-pounce-certfp-oftc.path;
-      join = "#vtluug,#vtluug-infra,#wuvt,#oftc";
-      away = "Currently disconnected from bouncer. A notification has been sent.";
-    };
-
-    notify =
-      let
-        notify-script = network: ''
-          NETWORK="${network}"
-          WEBHOOK=$(cat "${config.sops.secrets.shanghai-pounce-webhook.path}")
-
-          if [ -z "$NOTIFY_CHANNEL" ]; then
-            TITLE="Private Message"
-            CONTEXT="$NOTIFY_NICK"
-          else
-            TITLE="Highlight"
-            CONTEXT="$NOTIFY_CHANNEL"
-          fi
-
-          ${pkgs.curl}/bin/curl \
-            -H "Content-Type: application/json" \
-            -d "{\"embeds\":[{\"title\":\"$TITLE | $NETWORK/$CONTEXT\",\"description\":\"$NOTIFY_TIME $NOTIFY_NICK: $NOTIFY_MESSAGE\"}]}" \
-            $WEBHOOK
-        '';
-
-      in {
-        libera.commands = notify-script "libera";
-        oftc.commands = notify-script "oftc";
+    networks = {
+      libera = {
+        config = {
+          host = "irc.libera.chat";
+          nick = "waves";
+          client-cert = config.sops.secrets.shanghai-pounce-certfp-libera.path;
+          sasl-external = true;
+          join = "#openbsd,#nixos";
+          away = away-msg;
+        };
+        notify.script = notify-script "libera";
       };
+
+      oftc = {
+        config = {
+          host = "irc.oftc.net";
+          nick = "waves";
+          client-cert = config.sops.secrets.shanghai-pounce-certfp-oftc.path;
+          join = "#vtluug,#vtluug-infra,#wuvt,#oftc";
+          away = away-msg;
+        };
+        notify.script = notify-script "oftc";
+      };
+    };
   };
 }
--- modules/pounce.nix
+++ modules/pounce.nix
@@ -102,226 +102,245 @@ in {
     };
 
     networks = mkOption {
-      type = types.attrsOf settingsFormat.type;
-      default = {};
-      example = {
-        libera = {
-          host = "irc.libera.chat";
-          port = 6697;
-          sasl-plain = "testname:password";
-          join = "#nixos,#nixos-dev";
-        };
-      };
-      description = lib.mdDoc ''
-        Attribute set of Pounce configurations. For information on what
-        options Pounce accepts, see the
-        [pounce(1)](https://git.causal.agency/pounce/about/pounce.1) manual
-        page.
-      '';
-    };
-
-    notify = mkOption {
       type = types.attrsOf (types.submodule {
         options = {
-          insecure = mkOption {
-            type = types.bool;
-            default = false;
+          config = mkOption {
+            type = settingsFormat.type;
+            default = {};
+            example = {
+              libera = {
+                host = "irc.libera.chat";
+                port = 6697;
+                sasl-plain = "testname:password";
+                join = "#nixos,#nixos-dev";
+              };
+            };
             description = lib.mdDoc ''
-              Disable certificate validation for connecting to the Pounce
-              instance. Overrides
-              {option}`services.pounce.notify.<name>.trust-cert`.
+              Pounce configuration to user for the network. For information on
+              what options Pounce accepts, see the
+              [pounce(1)](https://git.causal.agency/pounce/about/pounce.1)
+              manual page.
             '';
           };
-          trust-cert = mkOption {
-            type = types.nullOr types.str;
-            default = "";
-            example = "/etc/letsencrypt/live/libera.irc.example.org/fullchain.pem";
-            description = lib.mdDoc ''
-              Pounce certificate for the pounce-notify client to trust.
-              This is required if Pounce is using a self-signed certificate.
-              If left blank, pounce-notify will use the appropriate
-              certificate in {option}`services.pounce.certDir`. Set to `null`
-              to disable certificate pinning.
-            '';
-          };
-          client-cert = mkOption {
-            type = types.str;
-            default = "";
-            description = lib.mdDoc ''
-              Client certificate to use if Pounce is configured to require
-              certificate authentication. If the relevant private key is stored
-              in a separate file, load it with
-              {option}`services.pounce.notify.<name>.client-priv`.
-            '';
-          };
-          client-priv = mkOption {
-            type = types.str;
-            default = "";
-            description = lib.mdDoc ''
-              Private key to use if Pounce is configured to require certificate
-              authentication. If the certificate provided in
-              {option}`services.pounce.notify.<name>.client-cert` has an
-              embedded private key, this option can be left empty.
-            '';
-          };
-          user = mkOption {
-            type = types.str;
-            default = "pounce-notify";
-            description = lib.mdDoc "Username to present to Pounce when connecting.";
-          };
-          commands = mkOption {
-            type = types.str;
-            default = "";
-            example = ''
-              # Pushover example
 
-              if [ -z "$NOTIFY_CHANNEL" ]; then
-                TITLE="Private Message"
-                CONTEXT="$NOTIFY_NICK"
-              else
-                TITLE="Mention"
-                CONTEXT="$NOTIFY_CHANNEL"
-              fi
+          notify = mkOption {
+            type = types.submodule {
+              options = {
+                insecure = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = lib.mdDoc ''
+                    Disable certificate validation for connecting to the Pounce
+                    instance. Overrides
+                    {option}`services.pounce.networks.<name>.notify.trust-cert`.
+                  '';
+                };
+                trust-cert = mkOption {
+                  type = types.nullOr types.str;
+                  default = "";
+                  example = "/etc/letsencrypt/live/libera.irc.example.org/fullchain.pem";
+                  description = lib.mdDoc ''
+                    Pounce certificate for the pounce-notify client to trust.
+                    This is required if Pounce is using a self-signed
+                    certificate. If left blank, pounce-notify will use the
+                    appropriate certificate in
+                    {option}`services.pounce.certDir`. Set to `null` to disable
+                    certificate pinning.
+                  '';
+                };
+                client-cert = mkOption {
+                  type = types.str;
+                  default = "";
+                  description = lib.mdDoc ''
+                    Client certificate to use if Pounce is configured to require
+                    certificate authentication. If the relevant private key is
+                    stored in a separate file, load it with
+                    {option}`services.pounce.networks.<name>.notify.client-priv`.
+                  '';
+                };
+                client-priv = mkOption {
+                  type = types.str;
+                  default = "";
+                  description = lib.mdDoc ''
+                    Private key to use if Pounce is configured to require
+                    certificate authentication. If the certificate provided in
+                    {option}`services.pounce.networks.<name>.notify.client-cert`
+                    has an embedded private key, this option can be left
+                    empty.
+                  '';
+                };
+                user = mkOption {
+                  type = types.str;
+                  default = "pounce-notify";
+                  description = lib.mdDoc ''
+                    Username to present to Pounce when connecting.
+                  '';
+                };
+                script = mkOption {
+                  type = types.lines;
+                  default = "";
+                  example = ''
+                    # Pushover example
 
-              $${pkgs.curl}/bin/curl \
-                -X POST \
-                --form-string token="API_TOKEN" \
-                --form-string user="USER_KEY" \
-                --form-string title="(libera/$CONTEXT) $TITLE" \
-                --form-string timestamp="$NOTIFY_TIME" \
-                --form-string message="$NOTIFY_NICK: $NOTIFY_MESSAGE" \
-                https://api.pushover.net/1/messages.json
-            '';
+                    if [ -z "$NOTIFY_CHANNEL" ]; then
+                      TITLE="Private Message"
+                      CONTEXT="$NOTIFY_NICK"
+                    else
+                      TITLE="Mention"
+                      CONTEXT="$NOTIFY_CHANNEL"
+                    fi
+
+                    $${pkgs.curl}/bin/curl \
+                      -X POST \
+                      --form-string token="API_TOKEN" \
+                      --form-string user="USER_KEY" \
+                      --form-string title="(libera/$CONTEXT) $TITLE" \
+                      --form-string timestamp="$NOTIFY_TIME" \
+                      --form-string message="$NOTIFY_NICK: $NOTIFY_MESSAGE" \
+                      https://api.pushover.net/1/messages.json
+                  '';
+                  description = lib.mdDoc ''
+                    Commands to run when a private message or a mention
+                    occurs. See
+                    [pounce-notify(1)](https://git.causal.agency/pounce/about/extra/notify/pounce-notify.1)
+                    for a list of environment variables containing information
+                    about the notification event.
+                  '';
+                };
+                command = mkOption {
+                  type = types.str;
+                  default = "";
+                  example = "/var/lib/pounce/notify.sh";
+                  description = lib.mdDoc ''
+                    Command to run when a private message or a mention occurs.
+                    Overrides
+                    {option}`services.pounce.notify.networks.<name>.notify.script`.
+                    See
+                    [pounce-notify(1)](https://git.causal.agency/pounce/about/extra/notify/pounce-notify.1)
+                    for a list of environment variables containing information
+                    about the notification event.
+                  '';
+                };
+              };
+            };
+            default = {};
+            example = { script = "/var/lib/pounce/notify.sh"; };
             description = lib.mdDoc ''
-              Series of commands to run when a private message or a mention
-              occurs. See
-              [pounce-notify(1)](https://git.causal.agency/pounce/about/extra/notify/pounce-notify.1)
-              for a list of environment variables containing information about
-              the notification event.
-            '';
-          };
-          script = mkOption {
-            type = types.str;
-            default = "";
-            example = "/var/lib/pounce/notify.sh";
-            description = lib.mdDoc ''
-              Script to run when a private message or a mention occurs.
-              Overrides {option}`services.pounce.notify.<name>.commands`. See
-              [pounce-notify(1)](https://git.causal.agency/pounce/about/extra/notify/pounce-notify.1)
-              for a list of environment variables containing information about
-              the notification event.
+              Configuration for pounce-notify. A notification client will be
+              spawned if either
+              {option}`services.pounce.networks.<name>.notify.script` or
+              {option}`services.pounce.networks.<name>.notify.command` is set.
             '';
           };
         };
       });
       default = {};
-      description = lib.mdDoc ''
-        Attribute set of notification clients to spawn. Each field must match a
-        network defined in {option}`services.pounce.networks`.
-      '';
+      description = lib.mdDoc "Attribute set of IRC networks to connect to.";
     };
   };
 
   config = mkIf cfg.enable {
     systemd.tmpfiles.rules = [ "d ${cfg.dataDir} 0700 ${cfg.user} ${cfg.user} -" ];
-    systemd.services = mkMerge [
-      {
-        calico = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
+    systemd.services = mkMerge (
+      [
+        {
+          calico = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
 
-          description = "Calico dispatcher for Pounce IRC bouncer.";
+            description = "Calico dispatcher for Pounce IRC bouncer.";
 
-          serviceConfig = {
-            User = cfg.user;
-            Group = cfg.user;
-            ExecStart = ''
-              ${pkgs.pounce}/bin/calico \
-                -H ${cfg.host} -P ${toString cfg.port} \
-                -t ${toString cfg.timeout} ${cfg.dataDir}
-            '';
-            Restart = "on-failure";
+            serviceConfig = {
+              User = cfg.user;
+              Group = cfg.user;
+              ExecStart = ''
+                ${pkgs.pounce}/bin/calico \
+                  -H ${cfg.host} -P ${toString cfg.port} \
+                  -t ${toString cfg.timeout} ${cfg.dataDir}
+              '';
+              Restart = "on-failure";
+            };
           };
-        };
-      }
+        }
+      ] ++ (mapAttrsToList (name: value: mkMerge [
+        {
+          "pounce-${name}" = {
+            wantedBy = [ "calico.service" ];
+            after = [ "network.target" ];
+            before = [ "calico.service" ];
 
-      (mapAttrs' (name: value: nameValuePair "pounce-${name}" {
-        wantedBy = [ "calico.service" ];
-        after = [ "network.target" ];
-        before = [ "calico.service" ];
+            description = "Pounce IRC bouncer for the ${name} network.";
 
-        description = "Pounce IRC bouncer for the ${name} network.";
+            serviceConfig = {
+              User = cfg.user;
+              Group = cfg.user;
+              ExecStart = ''
+                ${pkgs.pounce}/bin/pounce \
+                  -C ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem \
+                  -K ${cfg.certDir}/${name}.${cfg.host}/privkey.pem \
+                  -U ${cfg.dataDir} -H ${name}.${cfg.host} \
+                  ${settingsFormat.generate "${name}.cfg" value.config}
+              '';
+              Restart = "on-failure";
+            };
+            preStart = ''
+              mkdir -p ${cfg.certDir}/${name}.${cfg.host}
 
-        serviceConfig = {
-          User = cfg.user;
-          Group = cfg.user;
-          ExecStart = ''
-            ${pkgs.pounce}/bin/pounce \
-              -C ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem \
-              -K ${cfg.certDir}/${name}.${cfg.host}/privkey.pem \
-              -U ${cfg.dataDir} -H ${name}.${cfg.host} \
-              ${settingsFormat.generate "${name}.cfg" value}
-          '';
-          Restart = "on-failure";
-        };
-        preStart = ''
-          mkdir -p ${cfg.certDir}/${name}.${cfg.host}
-
-          if ${boolToString cfg.generateCerts}; then
-            if [ ! -f ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem ] || \
-               [ ! -f ${cfg.certDir}/${name}.${cfg.host}/privkey.pem ]; then
-              ${pkgs.libressl}/bin/openssl req -x509 -newkey rsa:4096 \
-                -out ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem \
-                -keyout ${cfg.certDir}/${name}.${cfg.host}/privkey.pem \
-                -nodes -sha256 -days 36500 -subj "/CN=${name}.${cfg.host}"
-            fi
-          fi
-        '';
-      }) cfg.networks)
-
-      (mapAttrs' (name: value:
-        assert (assertMsg (cfg.networks ? "${name}")
-          "Cannot listen for notifications on ${name}: network does not exist.");
-        nameValuePair "pounce-${name}-notify" {
-          wantedBy = [ "multi-user.target" ];
-          requires = [ "calico.service" "pounce-${name}.service" ];
-          after = [ "calico.service" "pounce-${name}.service" ];
-
-          description = "Pounce notification client for the ${name} network.";
-
-          serviceConfig = {
-            User = cfg.user;
-            Group = cfg.user;
-            Environment = "SHELL=${pkgs.bash}/bin/bash";
-            ExecStart = ''
-              ${pkgs.pounce-extra}/bin/pounce-notify \
-                ${if value.insecure then "-!" else ""} \
-                ${if value.client-cert != "" then "-c ${value.client-cert}" else ""} \
-                ${if value.client-priv != "" then "-k ${value.client-priv}" else ""} \
-                -p ${toString cfg.port} \
-                ${if !value.insecure && value.trust-cert != null then
-                  if value.trust-cert == "" then
-                    "-t ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem" else
-                    "-t ${value.trust-cert}" else ""} \
-                -u ${value.user} \
-                ${if cfg.networks.${name} ? local-pass
-                  then "-w cfg.networks.${name}.local-pass" else ""} \
-                ${name}.${cfg.host} \
-                ${if value.script != "" then value.script else
-                  pkgs.writeShellScript "pounce-${name}-notify-commands" value.commands}
+              if ${boolToString cfg.generateCerts}; then
+                if [ ! -f ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem ] || \
+                   [ ! -f ${cfg.certDir}/${name}.${cfg.host}/privkey.pem ]; then
+                  ${pkgs.libressl}/bin/openssl req -x509 -newkey rsa:4096 \
+                    -out ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem \
+                    -keyout ${cfg.certDir}/${name}.${cfg.host}/privkey.pem \
+                    -nodes -sha256 -days 36500 -subj "/CN=${name}.${cfg.host}"
+                fi
+              fi
             '';
-            Restart = "on-failure";
+          };
+        }
 
-            # pounce will refuse all connections before it's connected to the
-            # IRC network, but there's no easy way for systemd to know when
-            # that's happened. The best I've come up with is starting
-            # pounce-notify anyways and retrying with a fairly long delay.
-            # This value works for me, hopefully it works for you too.
-            RestartSec = "15s";
+        (mkIf (value.notify.command != "" || value.notify.script != "") {
+          "pounce-${name}-notify" = {
+            wantedBy = [ "multi-user.target" ];
+            requires = [ "calico.service" "pounce-${name}.service" ];
+            after = [ "calico.service" "pounce-${name}.service" ];
+
+            description = "Pounce notification client for the ${name} network.";
+
+            serviceConfig = {
+              User = cfg.user;
+              Group = cfg.user;
+              Environment = "SHELL=${pkgs.bash}/bin/bash";
+              ExecStart = ''
+                ${pkgs.pounce-extra}/bin/pounce-notify \
+                   ${if value.notify.insecure then "-!" else
+                     if value.notify.trust-cert == "" then
+                       "-t ${cfg.certDir}/${name}.${cfg.host}/fullchain.pem"
+                     else if value.notify.trust-cert != null then
+                       "-t ${value.notify.trust-cert}" else ""} \
+                   ${if value.notify.client-cert != "" then "-c ${value.notify.client-cert}" else ""} \
+                   ${if value.notify.client-priv != "" then "-k ${value.notify.client-priv}" else ""} \
+                   -p ${toString cfg.port} \
+                   -u ${value.notify.user} \
+                   ${if cfg.networks.${name}.config ? local-pass then
+                     "-w ${cfg.networks.${name}.config.local-pass}" else ""} \
+                   ${name}.${cfg.host} \
+                   ${if value.notify.command != "" then "\"${value.notify.command}\"" else
+                     pkgs.writeShellScript "pounce-${name}-notify-script" value.notify.script}
+              '';
+              Restart = "on-failure";
+
+              # pounce will refuse all connections before it's connected to the
+              # IRC network, but there's no easy way for systemd to know when
+              # that's happened. The best I've come up with is starting
+              # pounce-notify anyways and retrying with a fairly long delay.
+              # This value works for me, hopefully it works for you too.
+              RestartSec = "15s";
+            };
           };
         })
-      cfg.notify)
-    ];
+      ]) cfg.networks)
+    );
 
     users = optionalAttrs (cfg.user == defaultUser) {
       users.${defaultUser} = {
