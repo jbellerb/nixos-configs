1fd14becf258e7f4ebdf802c8ea10fa3aabea925
jae beller <foss@jae.zone>
2025-02-10 06:39:44 -0500

    shanghai: jail git in nspawn container to allow root-level repo paths

--- hosts/shanghai/modules/git.nix
+++ hosts/shanghai/modules/git.nix
@@ -1,14 +1,75 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  home = "/var/lib/git";
+  uid = 1500;
+
+in
 {
   users.users.git = {
     isNormalUser = true;
-    useDefaultShell = true;
-    home = "/home/git";
-    uid = 1500;
+    inherit home uid;
     openssh.authorizedKeys.keys = with config.metadata.hosts; [
       lagos.ssh_pubkey
       tugboat.ssh_pubkey
     ];
   };
+
+  containers.git = {
+    ephemeral = true;
+    autoStart = true;
+    config =
+      { pkgs, ... }:
+      {
+        system.stateVersion = "24.11";
+
+        environment.systemPackages = with pkgs; [ git ];
+
+        users.users.git = {
+          isNormalUser = true;
+          inherit home uid;
+          shell = "${pkgs.git}/bin/git-shell";
+        };
+
+        systemd.services.link-repos = {
+          wantedBy = [ "multi-user.target" ];
+
+          description = "Create symlinks to each git repo on root.";
+
+          serviceConfig.Type = "oneshot";
+          script = ''
+            for repo in ${home}/*
+            do
+              ln -s "''$repo" "/''${repo##*/}"
+              ln -s "''$repo" "/''${repo##*/}.git"
+            done
+          '';
+        };
+      };
+
+    bindMounts."/var/lib/git" = {
+      hostPath = "/var/lib/git";
+      isReadOnly = false;
+    };
+  };
+
+  services.openssh.extraConfig = ''
+    Match User git
+      ForceCommand sudo systemd-run --machine=git --uid=${builtins.toString uid} --pipe --wait --quiet /run/current-system/sw/bin/git-shell -c "\''$SSH_ORIGINAL_COMMAND"
+  '';
+
+  security.sudo.extraRules = [
+    {
+      users = [ "git" ];
+      runAs = "root";
+      commands = [
+        {
+          command = ''
+            /run/current-system/sw/bin/systemd-run --machine\=git --uid\=${builtins.toString uid} --pipe --wait --quiet /run/current-system/sw/bin/git-shell -c*
+          '';
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 }
